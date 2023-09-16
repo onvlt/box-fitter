27a1cc745925becda6ff77026e3920438bf9de54
Ondřej Nývlt <ondrej@nyv.lt>
2023-09-16 23:02:47 +0200

    WIP

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,145 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import * as THREE from "three";
+
+  // Data
+
+  let zoom = 2;
+  let rotation = {
+    x: 0,
+    y: 0,
+    z: 0,
+  };
+  let containerDimensions = {
+    width: 3,
+    height: 1,
+    depth: 2,
+  };
+
+  let itemDimensions = {
+    width: 0.8,
+    height: 1.5,
+    depth: 1,
+  };
+
+  // Scene
+
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  function makeGeometry(w, h, d) {
+    return new THREE.EdgesGeometry(new THREE.BoxGeometry(w, h, d));
+  }
+
+  const material = new THREE.LineBasicMaterial({
+    color: 0x00ff00,
+  });
+
+  const container = new THREE.LineSegments(
+    makeGeometry(
+      containerDimensions.width,
+      containerDimensions.height,
+      containerDimensions.depth
+    ),
+    material
+  );
+
+  scene.add(container);
+
+  $: {
+    camera.position.z = zoom;
+    container.rotation.x = rotation.x;
+    container.rotation.y = rotation.y;
+    container.rotation.z = rotation.z;
+
+    container.geometry.dispose();
+    container.geometry = makeGeometry(
+      containerDimensions.width,
+      containerDimensions.height,
+      containerDimensions.depth
+    );
+
+    renderer.render(scene, camera);
+  }
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <fieldset>
+    <legend>Camera</legend>
+    <label>
+      Zoom
+      <input bind:value={zoom} type="range" min="1.5" max="50" step="0.1" />
+    </label>
+    <label>
+      Rotation x:
+      <input
+        bind:value={rotation.x}
+        type="range"
+        min="-1"
+        max="1"
+        step="0.01"
+      />
+    </label>
+    <label>
+      Rotation y:
+      <input
+        bind:value={rotation.y}
+        type="range"
+        min="-1"
+        max="1"
+        step="0.01"
+      />
+    </label>
+    <label>
+      Rotation z:
+      <input
+        bind:value={rotation.z}
+        type="range"
+        min="-1"
+        max="1"
+        step="0.01"
+      />
+    </label>
+  </fieldset>
+  <fieldset>
+    <legend>Container</legend>
+    <label
+      >Width: <input
+        type="number"
+        bind:value={containerDimensions.width}
+      /></label
+    >
+    <label
+      >Height: <input
+        type="number"
+        bind:value={containerDimensions.height}
+      /></label
+    >
+    <label
+      >Depth: <input
+        type="number"
+        bind:value={containerDimensions.depth}
+      /></label
+    >
+  </fieldset>
+  <fieldset>
+    <legend>Item</legend>
+    <label
+      >Width: <input type="number" bind:value={itemDimensions.width} /></label
+    >
+    <label
+      >Height: <input type="number" bind:value={itemDimensions.height} /></label
+    >
+    <label
+      >Depth: <input type="number" bind:value={itemDimensions.depth} /></label
+    >
+  </fieldset>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
