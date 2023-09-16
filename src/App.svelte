64f49970f00cc0e53cf923f1f966004297432b3c
Ondřej Nývlt <ondrej@nyv.lt>
2023-09-16 23:44:10 +0200

    Stuff

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,26 @@
 <script lang="ts">
   import * as THREE from "three";
 
+  interface Dimensions {
+    width: number;
+    height: number;
+    depth: number;
+  }
+
   // Data
 
-  let zoom = 2;
+  let zoom = 5;
   let rotation = {
-    x: 0,
-    y: 0,
-    z: 0,
+    x: 0.3,
+    y: 0.6,
+    z: -Math.PI,
   };
-  let containerDimensions = {
+  let containerDimensions: Dimensions = {
     width: 3,
     height: 1,
     depth: 2,
   };
-
-  let itemDimensions = {
+  let itemDimensions: Dimensions = {
     width: 0.8,
     height: 1.5,
     depth: 1,
@@ -35,36 +40,64 @@
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
-  function makeGeometry(w, h, d) {
-    return new THREE.EdgesGeometry(new THREE.BoxGeometry(w, h, d));
-  }
+  const containerBox = new THREE.LineSegments();
+  containerBox.material = new THREE.LineBasicMaterial({ color: 0x00ff00 });
+  containerBox.position.set(0, 0, 0);
+  // containerBox.translateX(containerDimensions.width / -2);
+  // containerBox.translateY(containerDimensions.height / -2);
+  // containerBox.translateZ(containerDimensions.depth / -2);
 
-  const material = new THREE.LineBasicMaterial({
-    color: 0x00ff00,
-  });
+  const itemBox = new THREE.LineSegments();
+  itemBox.material = new THREE.LineBasicMaterial({ color: 0xff0000 });
+  itemBox.position.set(0, 0, 0);
+  // itemBox.translateX(itemDimensions.width / -2);
+  // itemBox.translateY(itemDimensions.height / -2);
+  // itemBox.translateZ(itemDimensions.depth / -2);
 
-  const container = new THREE.LineSegments(
-    makeGeometry(
-      containerDimensions.width,
-      containerDimensions.height,
-      containerDimensions.depth
-    ),
-    material
-  );
+  const group = new THREE.Group();
+  group.add(containerBox);
+  group.add(itemBox);
 
-  scene.add(container);
+  scene.add(group);
 
   $: {
     camera.position.z = zoom;
-    container.rotation.x = rotation.x;
-    container.rotation.y = rotation.y;
-    container.rotation.z = rotation.z;
+    group.rotation.x = rotation.x;
+    group.rotation.y = rotation.y;
+    group.rotation.z = rotation.z;
 
-    container.geometry.dispose();
-    container.geometry = makeGeometry(
-      containerDimensions.width,
-      containerDimensions.height,
-      containerDimensions.depth
+    const containerBoxGeometry = new THREE.EdgesGeometry(
+      new THREE.BoxGeometry(
+        containerDimensions.width,
+        containerDimensions.height,
+        containerDimensions.depth
+      )
+    );
+
+    const itemBoxGeometry = new THREE.EdgesGeometry(
+      new THREE.BoxGeometry(
+        itemDimensions.width,
+        itemDimensions.height,
+        itemDimensions.depth
+      )
+    );
+
+    containerBox.geometry.dispose();
+    containerBox.geometry = containerBoxGeometry;
+
+    itemBox.geometry.dispose();
+    itemBox.geometry = itemBoxGeometry;
+
+    containerBox.position.set(
+      containerDimensions.width / -2,
+      containerDimensions.height / -2,
+      containerDimensions.depth / -2
+    );
+
+    itemBox.position.set(
+      itemDimensions.width / -2,
+      itemDimensions.height / -2,
+      itemDimensions.depth / -2
     );
 
     renderer.render(scene, camera);
@@ -83,63 +116,87 @@
       <input
         bind:value={rotation.x}
         type="range"
-        min="-1"
-        max="1"
+        min={-Math.PI}
+        max={Math.PI}
         step="0.01"
       />
+      {rotation.x}
     </label>
     <label>
       Rotation y:
       <input
         bind:value={rotation.y}
         type="range"
-        min="-1"
-        max="1"
+        min={-Math.PI}
+        max={Math.PI}
         step="0.01"
       />
+      {rotation.y}
     </label>
     <label>
       Rotation z:
       <input
         bind:value={rotation.z}
         type="range"
-        min="-1"
-        max="1"
+        min={-Math.PI}
+        max={Math.PI}
         step="0.01"
       />
+      {rotation.z}
     </label>
   </fieldset>
   <fieldset>
     <legend>Container</legend>
-    <label
-      >Width: <input
+    <label>
+      Width: <input
         type="number"
         bind:value={containerDimensions.width}
-      /></label
-    >
-    <label
-      >Height: <input
+        min="0"
+        step="0.5"
+      />
+    </label>
+    <label>
+      Height: <input
         type="number"
         bind:value={containerDimensions.height}
-      /></label
-    >
-    <label
-      >Depth: <input
+        min="0"
+        step="0.5"
+      />
+    </label>
+    <label>
+      Depth: <input
         type="number"
         bind:value={containerDimensions.depth}
-      /></label
-    >
+        min="0"
+        step="0.5"
+      />
+    </label>
   </fieldset>
   <fieldset>
     <legend>Item</legend>
-    <label
-      >Width: <input type="number" bind:value={itemDimensions.width} /></label
-    >
-    <label
-      >Height: <input type="number" bind:value={itemDimensions.height} /></label
-    >
-    <label
-      >Depth: <input type="number" bind:value={itemDimensions.depth} /></label
-    >
+    <label>
+      Width: <input
+        type="number"
+        bind:value={itemDimensions.width}
+        min="0"
+        step="0.5"
+      />
+    </label>
+    <label>
+      Height: <input
+        type="number"
+        bind:value={itemDimensions.height}
+        min="0"
+        step="0.5"
+      />
+    </label>
+    <label>
+      Depth: <input
+        type="number"
+        bind:value={itemDimensions.depth}
+        min="0"
+        step="0.5"
+      />
+    </label>
   </fieldset>
 </main>
